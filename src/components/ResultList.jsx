240b4ecde8e1fc275c3fcea69236d6f7d92479ce
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-25 12:08:06 +0200

    Add pulse effect in loading

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -34,7 +34,14 @@ function ResultList() {
     }, [inputValue]);
 
     if(loading) {
-        return <h1>Loading...</h1>
+        return (
+            <div className='flex flex-col space-y-3 animate-pulse p-4 container mx-auto max-w-2xl'>
+                <div className='h-6 bg-gray-300 mt-5 rounded-md'></div>
+                <div className='h-40 bg-gray-300 mt-5 rounded-md'></div>
+                <div className='h-8 bg-gray-300 mt-5 rounded-md'></div>
+                <div className='h-40 bg-gray-300 mt-5 rounded-md'></div>
+            </div>
+        );
     }
 
     if(error) {
