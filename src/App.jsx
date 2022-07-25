375e806eabc216ded6d5562c916be6a9fbe707bf
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-25 10:32:52 +0200

    Add context in Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import "../src/App.css"
-import Header from './components/Header'
+import React, { createContext, useState } from 'react'
+import "../src/App.css";
+import Header from './components/Header';
+
+// Create context :
+export const InputContext = createContext();
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue)
+  const value = {
+    inputValue, 
+    setInputValue,
+  }
+
   return (
-    <div>
-      <Header />    
-    </div>
+    <InputContext.Provider value={value}>
+      <div className='App'>
+        <Header />    
+      </div>
+    </InputContext.Provider>
   )
 }
 
